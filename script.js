0aebe051d7f6e57b64f60664c543d10b3a8e35d0
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2023-09-16 22:48:17 -0400

    added to buttons in java

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ var startButton = document.querySelector("#startbutton")
 //second query shows the next div
 startButton.addEventListener("click", function(){
     document.querySelector("#startPage").style="display:none"
-    document.querySelector("#Quiz").style="display:block"
+    document.querySelector("#Question1").style="display:block"
 })
+//setting variable for quiz page 
+var Question1 = document.querySelector("#Question1")
+
+//adding click function to questions
+Question1.addEventListener("click", function(){
+    document.querySelector("#Question1").style="display:none"
+    document.querySelector("#Question2").style="display:block"
+})
+
+var Question2 = document.querySelector("#Question2")
+
+Question2.addEventListener("click", function(){
+    Question2.style="display:none"
+    document.querySelector("#initials").style="display:block"
+})
+
