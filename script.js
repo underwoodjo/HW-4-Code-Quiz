e541942d9935f9085f5e9dcecc73340413b051d8
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2023-09-16 20:59:15 -0400

    edited html started java script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-class CodeQuiz {
-    public static void main(String[] args) {
-        
-    }
-}
+//setting variable for start button from html
+var startButton = document.querySelector("#startbutton")
+//adding the click function to start button
+//first query hides the start page div
+//second query shows the next div
+startButton.addEventListener("click", function(){
+    document.querySelector("#startPage").style="display:none"
+    document.querySelector("#Quiz").style="display:block"
+})
