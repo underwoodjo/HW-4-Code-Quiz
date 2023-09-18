1a64b6e6e9583227d104a331bcc784d535a37bed
underwoodjo <132414633+underwoodjo@users.noreply.github.com>
2023-09-17 23:09:29 -0400

    editd button and created high score page

--- script.js
+++ script.js
@@ -11,15 +11,35 @@ startButton.addEventListener("click", function(){
 var Question1 = document.querySelector("#Question1")
 
 //adding click function to questions
-Question1.addEventListener("click", function(){
+var True1 = document.querySelector("#True1")
+var False1 = document.querySelector("#False1")
+True1.addEventListener("click", function(){
     document.querySelector("#Question1").style="display:none"
     document.querySelector("#Question2").style="display:block"
 })
-
+False1.addEventListener("click", function(){
+    document.querySelector("#Question1").style="display:none"
+    document.querySelector("#Question2").style="display:block"
+})
+var True2 = document.querySelector("#True2")
+var False2 = document.querySelector("#False2")
+True2.addEventListener("click", function(){
+    document.querySelector("#Question1").style="display:none"
+    document.querySelector("#Question2").style="display:block"
+})
+False2.addEventListener("click", function(){
+    document.querySelector("#Question2").style="display:none"
+    document.querySelector("#endofquiz").style="display:block"
+})
 var Question2 = document.querySelector("#Question2")
 
-Question2.addEventListener("click", function(){
-    Question2.style="display:none"
-    document.querySelector("#initials").style="display:block"
+var endofquiz = document.querySelector("#endofquiz")
+
+var submitInitials = document.querySelector("#submitInitials")
+    
+submitInitials.addEventListener("click", function(){
+    document.querySelector("#endofquiz").style="display:none"
+    document.querySelector("#highscores").style="display:block"
 })
+
 
